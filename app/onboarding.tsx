d18c5696d1f74986cd8c16bfe4d812eb822f7b43
nur-sh <nsheikhmohammed01@gmail.com>
2025-02-20 15:14:11 -0500

    Add gradient and image to onboarding screen

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,32 +1,58 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { theme } from "@/theme";
 import { useUserStore } from "@/store/userStore";
 import { useRouter } from "expo-router";
 import { PlantlyButton } from "@/components/PlantlyButton";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
+import { PlantlyImage } from "@/components/PlantlyImage";
 
 export default function OnboardingScreen() {
-
   const router = useRouter();
-  const toggleHasOnboarded = useUserStore(state => state.toggleHasOnboarded);
+  const toggleHasOnboarded = useUserStore((state) => state.toggleHasOnboarded);
 
   const handlePress = () => {
     toggleHasOnboarded();
-    router.replace('/');
-  }
+    router.replace("/");
+  };
 
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      style={styles.container}
+      colors={[theme.colorGreen, theme.colorAppleGreen, theme.colorLimeGreen]}
+    >
+      <StatusBar style="light" />
+      <View>
+
+      <Text style={styles.heading}>Plantly</Text>
+      <Text style={styles.tagline}>Keep your plants healthy and hydrated</Text>
+      </View>
+      <PlantlyImage />
       <PlantlyButton title="Let me in!" onPress={handlePress} />
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "space-evenly",
     alignItems: "center",
     backgroundColor: theme.colorWhite,
+  },
+  heading: {
+    fontSize: 42,
+    color: theme.colorWhite,
+    fontWeight: 'bold',
+    marginBottom: 12,
+    textAlign: 'center'
+  },
+  tagline: {
+    fontSize: 24,
+    color: theme.colorWhite,
+    textAlign: 'center'
   }
 });
