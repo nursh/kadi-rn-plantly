967cc29ce8ab527d84cc8bf9e7332bf602e760d1
nur-sh <nsheikhmohammed01@gmail.com>
2025-02-21 12:06:53 -0500

    Add plants list to screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,37 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { PlantCard } from "@/components/PlantCard";
+import { PlantlyButton } from "@/components/PlantlyButton";
+import { usePlantStore } from "@/store/plantStore";
+import { theme } from "@/theme";
+import { useRouter } from "expo-router";
+import React from "react";
+import { StyleSheet, FlatList } from "react-native";
 
 export default function Index() {
+  const plants = usePlantStore((store) => store.plants);
+  const router = useRouter();
 
   return (
-    <View style={styles.container}>
-      <Text>Our new app is up and running</Text>
-    </View>
-  )
+    <FlatList
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      data={plants}
+      renderItem={({ item }) => <PlantCard plant={item} />}
+      ListEmptyComponent={
+        <PlantlyButton
+          title="Add your first plant"
+          onPress={() => router.navigate("/new")}
+        />
+      }
+    />
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
+    backgroundColor: theme.colorWhite,
+  },
+  contentContainer: {
+    padding: 12,
+  },
+});
