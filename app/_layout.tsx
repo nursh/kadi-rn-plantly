bd0a59a6ef4d4bef799d3954cdabd05cf8ee076c
nur-sh <nsheikhmohammed01@gmail.com>
2025-02-24 20:37:44 -0500

    Add quick-action deep-linking

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Stack } from "expo-router";
+import * as QuickActions from 'expo-quick-actions';
+import { Platform } from 'react-native';
+import { useQuickActionRouting } from 'expo-quick-actions/router';
 
 
 export default function RootLayout() {
+  useQuickActionRouting();
+
+  useEffect(() => {
+    QuickActions.setItems([
+      {
+        title: "Add a plant",
+        icon: Platform.OS === 'ios' ? 'symbol:leaf' : 'leaf',
+        id: "0",
+        params: { href: "/new" }
+      }
+    ])
+  }, [])
 
   return (
     <Stack>
